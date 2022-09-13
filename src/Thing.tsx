77fee51c89e6f7002c7677b5e8cb6c7343dc9bc9
Nestor Mamani <nmamanipantoja@gmail.com>
2022-09-13 11:00:11 -0500

    Components change

---
+++ src/Thing.tsx
@@ -0,0 +1,19 @@
+import React, { FC, HTMLAttributes, ReactChild } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
+  children?: ReactChild;
+}
+
+// Please do not use types off of a default export module or else Storybook Docs will suffer.
+// see: https://github.com/storybookjs/storybook/issues/9556
+/**
+ * A custom Thing component. Neat!
+ */
+export const Thing: FC<Props> = ({ children }) => {
+  return (
+    <div className="p-4 rounded-lg bg-blue-300 font-bold text-blue-600">
+      {children || `the snozzberries taste like snozzberries`}
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,4 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import './tailwind.css';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return (
-    <div className="p-4 rounded-lg bg-blue-300 font-bold text-blue-600">
-      {children || `the snozzberries taste like snozzberries`}
-    </div>
-  );
-};
+export * from './Thing';
+export * from './Button';
